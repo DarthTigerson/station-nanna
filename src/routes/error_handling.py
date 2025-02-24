617b371e59a5f60a44275aea8dd38ab431bac696
Thomas Bonnici <thomas.bonnici@icloud.com>
2025-02-24 23:41:10 +0100

    wip error handling

--- src/routes/error_handling.py
+++ src/routes/error_handling.py
@@ -8,7 +8,7 @@ router = APIRouter(
     tags=["Error Handling"]
 )
 
-@router.get("/check_internet")
+@router.get("/check_internet", status_code=status.HTTP_200_OK)
 async def check_internet():
     try:
         response = requests.get('https://www.google.com', timeout=5)
@@ -16,11 +16,22 @@ async def check_internet():
         return {"message": "Internet connection is active"}
     except requests.RequestException as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
     
-@router.get("/check_wifi")
+@router.get("/check_wifi", status_code=status.HTTP_200_OK)
 async def check_wifi():
     try:
         os.system("nmcli dev wifi")
         return {"message": "Wifi connection is active"}
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+
+@router.get("/validate_station_url", status_code=status.HTTP_200_OK)
+async def validate_station_url(station_url: str):
+    try:
+        response = requests.get(station_url, timeout=5)
+        response.raise_for_status()
+        return {"message": "Station URL is valid"}
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
